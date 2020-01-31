00d119ecf4997bb3844fb29003e9c4d4e0d5027c
usw344@ubuntu18.04 <usw873pk@gmail.com>
2020-01-31 14:51:31 -0600

    Added Factorial stub function

---
+++ Lab 3/fact.py
@@ -0,0 +1,14 @@
+# CMPT 145 Lab03: Version Control
+#
+def factorial( n ):
+    """
+    Purpose :
+    Calculate the factorial of a non - negative integer
+    Pre - conditions :
+    n : non - negative integer
+    Return :
+    a non - negative integer
+    """
+    pass
+
+
